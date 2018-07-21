5a37e19f970a864c53c2a1ca3846652edf018a0f
David Ma <davidma@uber.com>
2018-07-21 13:22:36 -0700

    added dialog

--- questions.py
+++ questions.py
@@ -63,10 +63,3 @@ def schedule_command():
     )
     return response
 
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
