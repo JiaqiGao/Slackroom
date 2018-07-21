8a2577389d0f1c9eeb5d8603851f9df725088b48
JiaqiGao <jiaqigao12@gmail.com>
2018-07-21 09:36:53 -0700

    Added script

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
 from slackclient import SlackClient
 
-slack_token = os.environ["SLACK_API_TOKEN"]
+slack_token = os.environ["TOKEN"]
 sc = SlackClient(slack_token)
 
+# plan: random grouping...
+#
+#
+
+# my own channel
 sc.api_call(
   "chat.postMessage",
-  channel="C0XXXXXX",
-  text="Hello from Python! :tada:"
+  channel="DBTNNDCHW",
+  text="Test from Jiaqi! :tada:"
 )
