a1860fcafddadee8464f1208bd2658c8d479fbb0
winsonluk <winson.luk@gmail.com>
2018-07-21 17:26:50 -0700

    wtf

--- slackroom-server/application.py
+++ slackroom-server/application.py
@@ -149,7 +149,7 @@ application = Flask(__name__)
 
 msg = "https://slack.com/api/chat.postMessage"
 info = "https://slack.com/api/channels.info"
-
+dialog = "https://slack.com/api/dialog.open"
 
 # create channel and returns id
 def create_channel(channel_name):
@@ -157,8 +157,8 @@ def create_channel(channel_name):
     params = {'token': slack_token_grouping, 'name': channel_name}
     channel_post = requests.post(url=create_channel, params=params)
     result = channel_post.json()
+    print(result)
     return result["channel"]["id"]
-
 
 # add user to channel
 def add_to_channel(userID, channelID):
@@ -168,14 +168,17 @@ def add_to_channel(userID, channelID):
     result = channel_invite_post.json()
     return True
 
-
 # send a message to channel
 def send_message(channelID, text):
     send_message = "https://slack.com/api/chat.postMessage"
     params = {'token': slack_token_grouping, 'channel': channelID, 'text': text}
     send_message_post = requests.post(url=send_message, params=params)
+   # sc.api_call(
+    #    "chat.postMessage",
+     #   channel=channelID,
+      #  text=text
+    #)
     return True
-
 
 # returns a list of lists, randoming grouping the members given a group size
 def random_grouping(members, size):
@@ -189,7 +192,6 @@ def random_grouping(members, size):
         groups.append(group)
     return groups
 
-
 # return name of user from user ID
 def get_name(userID):
     mem_param = {'token': slack_token_grouping, 'user': userID}
@@ -199,52 +201,55 @@ def get_name(userID):
 
 ##################################################################
 
+def generate_team_name():
+    with open('adjectives.txt', "r",encoding='utf-8', errors='ignore') as f:
+        adj = f.read().split("\n")
+    with open('nouns.txt', "r",encoding='utf-8', errors='ignore') as f:
+        noun = f.read().split("\n")
+    return random.choice(adj) + "-" + random.choice(noun)
 
-def form_group(members, size):
-    grouped = random_grouping(members, size)  # "".join(map(str, random_grouping(members, 2)))
 
-    msg = "Here are " + str(len(grouped)) + " groups of " + str(len(grouped[0])) + " students: "
+
+def grouping(members, size):
+    grouped = random_grouping(members, size) #"".join(map(str, random_grouping(members, 2)))
+
+    msg = "*Here are " + str(len(grouped)) + " teams of " + str(len(grouped[0])) + " students:*\n"
     for group in range(len(grouped)):
         for student in range(len(grouped[group])):
-            if (student < len(grouped[0]) - 1):
-                msg += get_name(grouped[group][student]) + " + "
+            if (student < len(grouped[0])-1):
+                msg += get_name(grouped[group][student]) + ", "
             else:
                 msg += get_name(grouped[group][student])
-        msg += ", "
+        msg += "\n  "
 
     send_message("CBTUY2DUY", msg)
 
-    channels = {}  # dict of channel ids: [memberids]
-    counter = random.randint(0, 100000)
+    channels = {} # dict of channel ids: [memberids]
+    counter = random.randint(0,10000)
     for group in grouped:
-        channel_id = create_channel("team" + str(counter))
+        channel_id = create_channel("team-"+str(counter))
         new_channel = []
         for student in group:
             add_to_channel(student, channel_id)
             new_channel.append(student)
         counter += 1
-        send_message(channel_id, "Your new team consists of " + ", ".join([get_name(x) for x in group]))
-        send_message(channel_id, "Create a team name!")
+        send_message(channel_id, "Your new team consists of " + ", ".join([get_name(x) for x in group])+"!")
+        send_message(channel_id, "✨ Rename this channel with a new team name! ✨ \n "
+                                 "_Can't think of one?_ How about `"+generate_team_name()+"`?")
         channels["channel_id"] = new_channel
 
-    # print(channels)
 
-
-def init():
+def form_group(size):
     channel = "CBVSC5QDC"
-
     PARAMS = {'token': slack_token_grouping, 'channel': channel}
-
     info_result = requests.get(url=info, params=PARAMS)
-
     data = info_result.json()
-
     members = data['channel']['members']
-    form_group(members, 3)
+    grouping(members, size)
+
 
 
 active_q = None
-
 
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -303,6 +308,12 @@ def hello_world():
     elif request.form.get('command') == "/stats" and active_q is not None:
         toReturn = active_q.display_stats()
 
+    elif request.form.get('command') == '/createthread':
+        sc.api_call("chat.postMessage",
+                token=slack_token,
+                channel='CBUMNC8HG',
+                text="Start asking questions!")
+
     elif request.form.get('command') == '/schedule':
         sc.api_call(
             "dialog.open",
@@ -319,7 +330,7 @@ def hello_world():
         )
     elif (request.form.get('command')) == '/random-groups':
         toReturn = 'Forming groups!'
-        init()
+        form_group(6)
     dictionary = {
         "text": toReturn
     }
